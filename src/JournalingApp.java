f78a3718014e5548355ec29b6e6521359bb37fcb
Yeasir Arafat Aronno <yeasir35-1501@diu.edu.bd>
2023-12-21 00:09:35 +0600

    Journaling App

--- src/JournalingApp.java
+++ src/JournalingApp.java
@@ -179,21 +179,25 @@ public class JournalingApp {
         System.out.print("\nEnter the timestamp of the entry you want to delete: ");
         String timestampToDelete = userInput.nextLine();
 
+        // Create a temporary file to store the updated journal entries
         try (BufferedReader reader = new BufferedReader(new FileReader(JOURNAL_FILE));
              FileWriter writer = new FileWriter(JOURNAL_FILE + ".tmp")) {
 
             String line;
             boolean entryFound = false;
 
+            // Read each line from the original file and write it to the temporary file
             while ((line = reader.readLine()) != null) {
+                // Check if the line contains the timestamp to delete (to find the entry)
                 if (line.contains(timestampToDelete)) {
                     entryFound = true;
                     System.out.println("Journal entry deleted successfully!");
                 } else {
-                    writer.write(line + "\n");
+                    writer.write(line + "\n"); // Write the line to the temporary file
                 }
             }
 
+            // If the entry to delete was not found, print an error message
             if (!entryFound) {
                 System.out.println("No entry found with the specified timestamp.");
             }
@@ -205,6 +209,8 @@ public class JournalingApp {
         // Rename the temporary file to the original file
         File originalFile = new File(JOURNAL_FILE);
         File tempFile = new File(JOURNAL_FILE + ".tmp");
+
+        // Rename the temporary file to the original file (to update the journal file)
         if (tempFile.renameTo(originalFile)) {
             System.out.println("Journal file updated successfully.");
         } else {
@@ -212,22 +218,28 @@ public class JournalingApp {
         }
     }
 
-    private static void searchEntries(Scanner scanner) {
+    // This method is used to search for journal entries by keyword
+    private static void searchEntries(Scanner userInput) {
         System.out.print("\nEnter the keyword to search for: ");
-        String keyword = scanner.nextLine().toLowerCase();
+        String keyword = userInput.nextLine().toLowerCase();
 
+        // Read the journal file line by line using a BufferedReader and print
+        // each line that contains the keyword (case-insensitive) to the console
         try (BufferedReader reader = new BufferedReader(new FileReader(JOURNAL_FILE))) {
             System.out.println("\n*** Search Results for '" + keyword + "' ***\n");
             String line;
             int entryCount = 0;
 
+            // Read each line from the file until the end of the file is reached
             while ((line = reader.readLine()) != null) {
+                // Check if the line contains the keyword (case-insensitive)
                 if (line.toLowerCase().contains(keyword)) {
                     System.out.println(line);
                     entryCount++;
                 }
             }
 
+            // If no entries were found, print an error message
             if (entryCount == 0) {
                 System.out.println("No entries found with the specified keyword.");
             }
@@ -237,17 +249,23 @@ public class JournalingApp {
         }
     }
 
+    // This method is used to export journal entries to an HTML file
     private static void exportToHTML() {
+        // Read the journal file line by line using a BufferedReader
+        // and write each line to an HTML file using a FileWriter
         try (BufferedReader reader = new BufferedReader(new FileReader(JOURNAL_FILE));
              FileWriter writer = new FileWriter(HTML_EXPORT_FILE)) {
 
+            // Write the HTML header to the file (to display the entries in a web browser)
             writer.write("<html><head><title>Journal Entries</title></head><body>");
 
             String line;
+            // Read each line from the file until the end of the file is reached
             while ((line = reader.readLine()) != null) {
                 writer.write(line + "<br>");
             }
 
+            // Write the HTML footer to the file (to display the entries in a web browser)
             writer.write("</body></html>");
             System.out.println("Journal entries exported to HTML successfully. Check 'journal_export.html'.");
 
@@ -256,10 +274,12 @@ public class JournalingApp {
         }
     }
 
-    private static void passwordProtectEntries(Scanner scanner) {
+    // This method is used to password protect journal entries in the journal file
+    private static void passwordProtectEntries(Scanner userInput) {
         System.out.print("\nEnter the password to protect the entries: ");
-        String enteredPassword = scanner.nextLine();
+        String enteredPassword = userInput.nextLine();
 
+        // Check if the entered password matches the password constant (PASSWORD)
         if (enteredPassword.equals(PASSWORD)) {
             System.out.println("Entries are now password protected.");
         } else {
